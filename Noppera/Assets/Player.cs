d4b04cd34e5247bdde817ca18190ca606740127c
kleinzach <henry.eastman@gmail.com>
2015-02-15 11:46:48 -0500

    Movement

---
+++ Noppera/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class Player : MonoBehaviour {
+	
+	public float moveSpeed;
+	
+	float h;
+	float v;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+		
+		rigidbody2D.MovePosition(this.rigidbody2D.position + new Vector2(h,v)*moveSpeed);
+	}
+}
