37337b44df033e30139cf14f588948982fc7b2a7
kleinzach <henry.eastman@gmail.com>
2015-04-04 16:00:01 -0400

    Added new animations

--- Noppera/Assets/Player.cs
+++ Noppera/Assets/Player.cs
@@ -6,11 +6,15 @@ using System.Collections;
 public class Player : MonoBehaviour {
 	
 	public float moveSpeed;
+
+	public Animator anim;
 	
 	float h;
 	float v;
 
 	bool right;
+	bool back;
+	bool idle;
 
 	public static Player instance;
 	
@@ -26,11 +30,14 @@ public class Player : MonoBehaviour {
 		
 		GetComponent<Rigidbody2D>().MovePosition(this.GetComponent<Rigidbody2D>().position + new Vector2(h,v)*moveSpeed);
 		bool lastorientation = right;
-		right = (h > 0);//||(lastorientation);
-
+		right = right?(h >= 0):(h > 0);//||(lastorientation);
+		idle = (Mathf.Abs(h) < .1f) && (Mathf.Abs(v) < .1f);
+		back = back?(v >= 0):(v > 0);
 		if(lastorientation != right){
 			this.transform.localScale = new Vector3((right)?-.5f:.5f,transform.localScale.y,transform.localScale.z);
 		}
+		anim.SetBool("idle", idle);
+		anim.SetBool("back", back);
 	}
 
 	public void warpTo(Vector2 v)
