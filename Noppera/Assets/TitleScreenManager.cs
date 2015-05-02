58218581d5cb8a96abed36ece39881011a926b62
John Billingsley <jmb330@case.edu>
2015-05-02 16:27:51 -0400

    added tree level and working on making main menu work

---
+++ Noppera/Assets/TitleScreenManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenManager : MonoBehaviour {
+
+	public void startGame(){
+		Application.LoadLevel("intro");
+	}
+	
+	public void exitGame(){
+		Application.Quit();
+	}
+	
+	public void testButton(){
+		Debug.Log("i got hella clicked doe");
+	}
+}
---
+++ Noppera/Assets/TreeRoom/Follow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().AddForce((target.transform.position - transform.position).normalized);
+	}
+}
